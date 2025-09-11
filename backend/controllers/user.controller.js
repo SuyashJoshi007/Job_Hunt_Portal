ff1db8dd5f47a3892eb48ead86ee14e649249747
SuyashJoshi007 <suyashjoshisj123@gmail.com>
2025-09-11 19:39:15 +0530

    fix: ensure secure cookie settings for login response

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -108,6 +108,7 @@ export const login = async (req, res) => {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpsOnly: true, 
         sameSite: process.env.NODE_ENV === "development" ? "strict" : "none",
+        secure: true
       })
       .json({
         message: `Welcome back ${user.fullname}`,
