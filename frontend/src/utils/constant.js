9240c40b7ee2f4e99e807201d636eeab7b0df46c
SuyashJoshi007 <suyashjoshisj123@gmail.com>
2025-09-11 18:54:10 +0530

    refactor: update API endpoints to use environment variable for backend URI

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,5 @@
-export const USER_API_END_POINT="http://localhost:8080/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8080/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8080/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8080/api/v1/company";
+const BACKEND_URI=import.meta.env.VITE_BACKEND_URI
+export const USER_API_END_POINT=`${BACKEND_URI}/api/v1/user`;
+export const JOB_API_END_POINT=`${BACKEND_URI}/api/v1/job`;
+export const APPLICATION_API_END_POINT=`${BACKEND_URI}/api/v1/application`;
+export const COMPANY_API_END_POINT=`${BACKEND_URI}/api/v1/company`;
